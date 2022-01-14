e3be37cfad00a155d857930bc88dcb92cc49ef2d
Angel Martinez <angel.martineztrc@gmail.com>
2022-01-14 17:30:59 -0600

    fix:  avoid create a field if label doesnt exists

--- src/components/FieldsForm.tsx
+++ src/components/FieldsForm.tsx
@@ -19,6 +19,8 @@ const FieldsForm = ({ fields, setFields }: FieldsFormProps) => {
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
 
+    if (!values.label) return;
+
     setFields([
       ...fields,
       {
